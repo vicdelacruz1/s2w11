27b0d0ac63448f8bf002238ef9aeb9b4ff50dd98
De La Cruz <vicdelacruz@FVFY377TJ1WK.tld>
2022-03-30 09:58:32 -0500

    second commit

--- twenty.js
+++ twenty.js
@@ -11,7 +11,7 @@ function dealer() {
         computer = computer + ran 
         alert(`The number is ${ran} and the current total is ${computer}`);
     } while(computer < 16);
-    return dealer
+    return computer
 }
 
 function player() {
@@ -23,9 +23,47 @@ function player() {
         alert(`The number is ${ran} and the current total is ${user}`);
         var choice = prompt("Draw again? Answer yes or no")
     } while(choice == "yes")
-    return player
+    return user
 }
 
+function compare() {
+    var p = player()
+    var d = dealer()
+    if (p <= 21) {
+        alert("Nice Draw");
+    } else if (p > 21){
+        alert("Yikes, you drew higher than a 21, you lose.");
+        break;
+    } else if (d <= 21) {
+        alert("This is going to be close");
+    } else if (d > 21){
+        alert("Ooh, the dealer drew higher than 21, you win!");
+        break;
+    } 
+    
+    return compare
+}
+
+function wCheck() {
+    var win = 0;
+    var lose = 0;
+    var p = player()
+    var d = dealer()
+    if (p > d) {
+        alert("You win");
+        win = win + 1;
+    } else {
+        alert("You lose")
+        lose = lose + 1;
+    }
+    alert(`Current wins: Player: ${win} , Dealer: ${lose}`)
+    return wCheck()
+}
+
+
+
+
+
 // askName()
-dealer()
-player()
+compare()
+wCheck()
